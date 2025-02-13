b8b82628d27a40eb72a811394fe6dc0d8e1dbedf
pradhanhitesh <hiteshpradhan@iitgn.ac.in>
2025-02-13 12:05:39 +0530

    added new functions

--- UTILS/extra.py
+++ UTILS/extra.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 import pytz
+import os
+import shutil
 
 def get_date_cycle(upper_range = 1, lower_range=8):
     # Cycle dates every 7 days
@@ -11,3 +13,15 @@ def get_date_cycle(upper_range = 1, lower_range=8):
     lower_str = lower.strftime("%Y/%m/%d")
 
     return lower_str, upper_str
+
+def delete_data(dir_path="DATA"):
+    # Remove all contents but keep the directory
+    for filename in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)  # Remove files and symlinks
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)  # Remove directories
+        except Exception as e:
+            print(f"Error deleting {file_path}: {e}")
---
+++ UTILS/oped.py
@@ -0,0 +1,173 @@
+import re
+from bs4 import BeautifulSoup
+import json
+import os
+from typing import Optional
+from google import genai
+from google.genai import types
+from jinja2 import Template
+from weasyprint import HTML
+from datetime import datetime
+import pytz
+
+class Oped():
+    def __init__(self):
+        pass
+
+    def _collect_metadata(self, soup):
+        # Extract headline from <meta> tag
+        headline = soup.find("meta", attrs={"name": "title"})
+        headline = headline["content"] if headline else "N/A"
+
+        # Extract publish date from <meta> tag
+        publish_date = soup.find("meta", attrs={"name": "publish-date"})
+        publish_date = publish_date["content"][:10] if publish_date else "N/A"
+
+        # Extract publish time from <meta> tag
+        publish_time = soup.find("meta", attrs={"name": "publish-date"})
+        publish_time = publish_time["content"][11:16] if publish_time else "N/A"
+
+        # Try extracting from JavaScript (dataLayer) if available
+        script_tag = soup.find("script", string=re.compile("dataLayer"))
+        if script_tag:
+            match = re.search(r"dataLayer\.push\((.*?)\);", script_tag.string, re.DOTALL)
+            if match:
+                try:
+                    data_layer = json.loads(match.group(1).replace("'", '"'))  # Convert JS-like to JSON
+                    page_details = data_layer.get("pageDetails", {})
+                    
+                    headline = page_details.get("headline", headline)
+                    publish_date = page_details.get("publishDate", publish_date)
+                    publish_time = page_details.get("publishTime", publish_time)
+                except json.JSONDecodeError:
+                    pass
+
+        return {
+                "Headline" : headline,
+                "Publication Date": publish_date,
+                "Publication Time": publish_time,
+                }
+    
+    def _llm(self, GEMINI_API_KEY: str, content: str, sys_instruct: str):
+        # Setup client
+        client = genai.Client(api_key=GEMINI_API_KEY)
+
+        response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+            system_instruction=sys_instruct,
+            max_output_tokens=500,
+            temperature=0.7),
+            contents=[content],)
+            
+        if "```json" in response.text:
+            # Clean the response by removing the code block markers
+            response_content_cleaned = response.text.replace("```json", "").replace("```", "").strip()
+            response_json = json.loads(response_content_cleaned)
+        else:
+            # Parse the response content back to a Python dictionary
+            response_json = json.loads(response.text)
+
+        return response_json
+    
+    def _extract_paragraph(self, soup, GEMINI_API_KEY: str):
+        # Initialize list
+        body_content = []
+
+        # Find the tag with itemprop="articleBody"
+        article_body = soup.find(attrs={"itemprop": "articleBody"})
+
+        # If the article body exists, find all <p> tags within it
+        if article_body:
+            paragraphs = article_body.find_all('p', recursive=False)
+
+            # Print the text of each paragraph
+            for i, p in enumerate(paragraphs, 1):
+                if 'Published - ' not in p.text.strip():
+                    body_content.append(p.text.strip())
+                else:
+                    body_content.append(p.text.strip())
+                    break
+        else:
+            print("No article body found with itemprop='articleBody'.")
+
+        body_content = " ".join(content for content in body_content)
+
+        # Summarize content
+        sys_instruct="""
+        You are an AI which helps in summarizing content.
+        It is very important that you summarize in just 500 words.
+        Based on user input, make sure to keep the essence of Op-Ed News Article.
+
+        Use this JSON Schema:
+        Content = {'summary': str}
+        Return: Content
+        """
+        content = self._llm(GEMINI_API_KEY, body_content, sys_instruct)
+
+        return body_content, content['summary']
+
+
+    def _process_page(self, soup: BeautifulSoup, GEMINI_API_KEY: str, output_path: Optional[str] = None, output_md=True):
+        if output_path != None:
+            if not os.path.exists(output_path):
+                raise ValueError(f"Specified path {output_path} does not exist.")
+        
+        # Assuming _collect_metadata and _extract_paragraph are defined elsewhere
+        metadata = self._collect_metadata(soup)
+        content, summary = self._extract_paragraph(soup, GEMINI_API_KEY)
+
+        # Store data in dict
+        page_data = {
+                "Metadata" : metadata,
+                "Content" : {
+                    "Original" : content,
+                    "Summary" : summary
+                }
+            }
+        
+        try:
+            if output_md and not output_path:
+                return page_data       
+            elif output_md and output_path:
+                filename = metadata.get("Headline",["No Headline"]).replace(" ","+")
+                output_path = os.path.join(output_path, f"{filename}.json")
+                with open(output_path, 'w') as file:
+                    json.dump(page_data, file)
+                return page_data
+        except Exception as e:
+            raise ValueError(f"Could not extract page data due to {e}")
+        
+
+    def _message(self, page_data: dict, template_path: str, output_path: str):
+        message_content = []
+
+        # MD message
+        for meta in page_data['Metadata']:
+            message_content.append(f"### **{meta}:** {page_data['Metadata'][meta]}\n")
+        message_content.append(f"**Summary:** {page_data['Content']['Summary']}")
+        message_content = "".join(content for content in message_content)
+
+        # PDF file
+        template_vars = {
+            'headline': page_data['Metadata']['Headline'],
+            'publication_date' : page_data['Metadata']['Publication Date'],
+            'publication_time' : page_data['Metadata']['Publication Time'],
+            'original' : page_data['Content']['Original'],
+            'summary': page_data['Content']['Summary'],
+            'generation_date' : datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%d-%m-%Y %H:%M')
+        }
+
+        # Read the template file directly
+        with open(template_path, 'r') as file:
+            template_content = file.read()
+        
+        # Render the template with the provided context
+        template = Template(template_content)
+        html_content = template.render(template_vars)
+
+        # Convert the rendered HTML to a PDF
+        output_path = os.path.join(output_path, f"{page_data['Metadata']['Headline'].replace(' ','+')}.pdf")
+        HTML(string=html_content).write_pdf(output_path)
+
+        return message_content, output_path
